b90acec913d522d691d2e208a9abeef92f9ba23d
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2021-12-15 17:51:25 +0100

    Error 403

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+Vue.config.devtools = true;
+
+const app = new Vue (
+    {
+        el:'#root',
+        data: {
+            randomEmail: null,
+        },
+        methods: {
+            
+        },
+        created: function() {
+                axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+                .then((response) => {
+                    const serverRandomEmail = response.data.response;
+                    this.randomEmail = serverRandomEmail;
+                    // console.log(response.data.response);
+                });
+        }
+    }
+);
+
+
+
+
+// axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+// .then((response) => {
+//     console.log(response.data.response);
+// })
